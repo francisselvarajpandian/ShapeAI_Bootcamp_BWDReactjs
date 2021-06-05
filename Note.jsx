49a4c2cb49877ab93ea70d69df85fd2e7011c014
francisselvarajpandian <62279634+francisselvarajpandian@users.noreply.github.com>
2021-06-05 16:43:50 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>About</h1>
+      <p>Ui/Ux Designer</p>
+    </div>
+  );
+}
+export default Note;
